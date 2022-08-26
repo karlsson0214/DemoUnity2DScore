9470200956ec81e7d44d324a13108ce315532abc
Karlsson0214 <karlsson0214@gmail.com>
2022-08-26 18:35:50 +0200

    using ...

--- Assets/Scripts/Crab.cs
+++ Assets/Scripts/Crab.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(CapsuleCollider2D))]
