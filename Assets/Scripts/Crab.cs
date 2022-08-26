18cd3dd0cb9929b396492f5b96d0ba1acc1144e9
Karlsson0214 <karlsson0214@gmail.com>
2022-08-26 18:35:10 +0200

    move crab with a s d

--- Assets/Scripts/Crab.cs
+++ Assets/Scripts/Crab.cs
@@ -2,17 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
+
 public class Crab : MonoBehaviour
 {
+    public Text scoreDisplay;
+
+    private Rigidbody2D rb;
+    private float speed = 4;
+    private int score = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        // Physics engine will not turn Crab
+        rb.freezeRotation = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.S))
+        {
+            // walk
+            float angleRadians = rb.rotation * Mathf.PI / 180f;
+            float xSpeed = Mathf.Cos(angleRadians) * speed;
+            float ySpeed = Mathf.Sin(angleRadians) * speed;
+            rb.velocity = new Vector2(xSpeed, ySpeed);
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                // and turn left
+                rb.rotation += 1;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                // and turn right
+                rb.rotation -= 1;
+            }
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
     }
 }
